69759f9b9e3efabf136225de72a372596db280d1
bobadepo03 <bobadepo03@gmail.com>
2026-06-20 05:28:31 +0700

    Update script.js
    update timestamp

--- js/script.js
+++ js/script.js
@@ -32,6 +32,18 @@ function getTanggalByBg(bgName){
   return formatDate(date);
 }
 
+function updateTimestamp(){
+    const now = new Date();
+    const options = { 
+        day: '2-digit', month: 'short', year: 'numeric',
+        hour: '2-digit', minute: '2-digit', second: '2-digit'
+    };
+    const formatted = now.toLocaleString('en-GB', options).replace(',', '');
+    document.getElementById("timestamp").textContent = formatted;
+}
+setInterval(updateTimestamp, 1000);
+updateTimestamp();
+
 /* =======================
    ASSETS
 ======================= */
